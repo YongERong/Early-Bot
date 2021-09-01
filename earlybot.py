e22e8e7649148d40120b2960e0a38fc371a7e420
YongERong <yong_e-rong@s2017.sst.edu.sg>
2021-09-01 13:56:01 +0800

    Version 0.3.0 size scrape, basic error checks and logs

--- earlybot.py
+++ earlybot.py
@@ -5,6 +5,8 @@ from discord.ext import commands
 import requests
 import re
 
+from time import perf_counter #log
+
 #headless, disable extensions, disable gpu, disable images
 chrome_options = Options()
 chrome_options.add_argument("--disable-extensions")
@@ -14,73 +16,55 @@ chrome_options.add_argument('--blink-settings=imagesEnabled=false')
 
 #on startup
 driver = webdriver.Chrome('/usr/local/bin/chromedriver', options=chrome_options)
-
-# Discord
 bot = commands.Bot(command_prefix="?", help_command=None, description="""Listening on ?help
 Developed by @noone""")
 
 
 @bot.command()
 async def snkrs(ctx:str, arg1:str, arg2:str, arg3:str):
-    if len(arg1) != 2:
+    start = perf_counter() #log
+    if len(arg1) != 2 or arg1.isascii() == False:
         await ctx.channel.send("Region is incorrect")
         raise
-    if len(arg2) < 5 or len(arg2) > 60:
+    if len(arg2) < 5 or len(arg2) > 60 or arg2.isascii() == False:
         await ctx.channel.send("Name is incorrect")
         raise
-    if len(arg3) < 1 or len(arg3) > 4:
+    if len(arg3) < 1 or len(arg3) > 4 or arg3.isascii() == False:
         await ctx.channel.send("Size is incorrect")
         raise
-
-
-
+    stop_errorcheck = perf_counter() #log
     url = "https://www.nike.com/" + arg1 + "/launch/t/" + arg2.replace(" ", "-")
-    print(url)
     driver.get(url)
-
     #Grab all items from network tab of google chrome developer tools into a list of dictionaries
     data = driver.execute_script("var performance = window.performance || window.mozPerformance || window.msPerformance || window.webkitPerformance || {}; var network = performance.getEntries() || {}; return network;")
     #Get the name of every dictionary within the list of dictionary in a list
     names = [obj['name'] for obj in data]
     #Product id links
     ilinks = [name for name in names if name.startswith("https://api.nike.com/merch/skus/v2/?filter=productId%")]
+    ilinks = list(set(ilinks))
     print(ilinks)
-    #Product ids and remove duplicates
-    ids = [link.removeprefix("https://api.nike.com/merch/skus/v2/?filter=productId%28") for link in ilinks]
-    ids = [link2.split("%",1)[0] for link2 in ids]
-    ids = list(set(ids))
-    print(ids)
-    if len(ids) != 1:
-        ids = []
-        sizes = []
-        for links in ilinks:
-            a = requests.get(links)
-            #b = ast.literal_eval(a.text)
-            #subids = [e.get('productId') for c in b for d in c for e in d]
-            #subsizes = [e.get('nikeSize') for c in b for d in c for e in d]
-            #subids = re.findall(r'productId" : "\w{8}-\w{4}-\w{4}-\w{4}-\w{12}',a.text,re.ASCII)
-            subsizes = [size.split('"')[2] for size in re.findall(r'nikeSize" : "\S{1,4}',a.text,re.ASCII)]
-            #print(subids)
-            print(subsizes)
-            #ids.append(subids)
-            #sizes.append(subsizes)
-            if arg3 in subsizes:
-                id = links.removeprefix("https://api.nike.com/merch/skus/v2/?filter=productId%28")
-                id = id.split("%",1)[0]
-                print(id)
-                break
-
-        #print(ids)
-        #print(sizes)
-        #id = ids[sizes.index(arg3)]
-    else:
-        id = ids[0]
-
-    elink = url + "?productId=" + id + "&size=" + arg3
-        
-        
-                
+    stop_selscrape = perf_counter() #log
+    for links in ilinks:
+        a = requests.get(links)
+        subsizes = [size.split('"')[2] for size in re.findall(r'nikeSize" : "\S{1,4}',a.text,re.ASCII)]
+        print(subsizes)
+        if arg3 in subsizes:
+            id = links.removeprefix("https://api.nike.com/merch/skus/v2/?filter=productId%28")
+            id = id.split("%",1)[0]
+            break
+    try:
+        elink = url + "?productId=" + id + "&size=" + arg3     
+    except(UnboundLocalError):
+        await ctx.channel.send("Size requested is not compatible with product")
+        raise
+    stop_apiscrape = perf_counter() #log
     await ctx.channel.send(elink)
+    #log
+    await ctx.channel.send("""
+    Total time taken: {}s
+    Time taken for error prevention: {}s
+    Time taken for Selenium scrape:{}s
+    Time taken for api scrape: {}s""".format(stop_apiscrape - start, stop_errorcheck - start, stop_selscrape - stop_errorcheck, stop_apiscrape - stop_selscrape))
 
 @bot.command()
 async def ping(ctx):
@@ -98,7 +82,7 @@ async def help(ctx):
 
 
 #remove token later
-bot.run("ODgxNTUxNzM0Nzg1MzIzMDM4.YSufCA.ftsBX1KZ0BiSpFBGNm1AqPLl1WM")
+bot.run("")
 
 
 
