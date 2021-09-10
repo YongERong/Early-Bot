6f26ecc23a7c91c3e4359d9ce36224292c785ed7
YongERong <yong_e-rong@s2017.sst.edu.sg>
2021-09-10 15:25:47 +0800

    Version 1.0.1 Added link to size chart

--- earlybot.py
+++ earlybot.py
@@ -67,7 +67,7 @@ async def ping(ctx):
 @bot.command()
 async def help(ctx):
     embed=discord.Embed(title="Early Bot Help", description="Version 1.0.0", color = discord.Colour.random())
-    embed.add_field(name="```?snkrs <Region> <name in quotes or with dashes> <size>```",value="Command for Nike SNKRS early link",inline=False)
+    embed.add_field(name="```?snkrs <Region> <Name in quotes or with dashes> < US size>```",value="Command for Nike SNKRS early link, size chart can be found [here](https://www.nike.com/sg/size-fit/mens-footwear)",inline=False)
     embed.add_field(name="```?ping```",value="Check if the bot is online",inline=False)
     await ctx.send(embed=embed)
 
